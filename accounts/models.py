8cbf7f1dcfa1b349aa4168ff9fe95143ff0261f4
Dipayan Dutta <inbox.dipayan@gmail.com>
2020-06-01 11:47:14 +0530

    Database Relationship , One to One and Many to Many Relationship added

--- accounts/models.py
+++ accounts/models.py
@@ -11,13 +11,11 @@ class Customer(models.Model):
     def __str__(self):
         return self.name
 
-
 # For many to many relationship
-
 class Tag(models.Model):
     name = models.CharField(max_length=200,null=True)
 
-    def __str__(sellf):
+    def __str__(self):
         return self.name
 
 
@@ -29,15 +27,18 @@ class Product(models.Model):
     name        = models.CharField(max_length=200,null=True) 
     price       = models.FloatField(null=True)
     category    = models.CharField(max_length=200,null=True,choices=CATEGORY)
-    description = models.CharField(max_length=200,null=True)
+    description = models.CharField(max_length=200,null=True,blank=True)
     date_created= models.DateTimeField(auto_now_add=True,null=True)
     # Add the Many to Many Relationship
     tags = models.ManyToManyField(Tag)
 
+    def __str__(self):
+        return self.name
 
 
+'''This is the Many  to One Relationship'''
 
-class Order(models.Model):
+class Order(models.Model): 
     '''
         For Drop Down Menu
     '''
@@ -53,4 +54,6 @@ class Order(models.Model):
     date_created = models.DateTimeField(auto_now_add=True,null=True)
     status       = models.CharField(max_length=200,null=True,choices=STATUS)
 
-   
+    
+    def __str__(self):
+        return self.customer
