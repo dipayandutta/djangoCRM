b5ec9673e4479d147d0bbffd799afe6eab6da566
Dipayan Dutta <inbox.dipayan@gmail.com>
2020-06-02 23:36:27 +0530

    CRUD operation Completed

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import home , products,customer,createOrder,updateOrder
+from .views import home , products,customer,createOrder,updateOrder,deleteOrder
 urlpatterns = [
     path('',home,name='home'),
     path('products/',products,name='products'),
     path('customer/<str:pk_test>/',customer,name='customer'),
     path('create_order/',createOrder,name='create_order'),
     path('update_order/<str:pk>/',updateOrder,name='update_order'),
+    path('delete_order/<str:pk>/',deleteOrder,name='delete_order'),
 ]
